164428830706b5f94d6ffd0cfc3b311359a10e73
Anzhelika Shilyaeva <ana.anajel@yandex.ru>
2023-04-09 21:40:42 +0300

    Пройти тесты

--- backend/app.js
+++ backend/app.js
@@ -19,7 +19,7 @@ const { createUser, login } = require('./controllers/users');
 const errorHandler = require('./middlewares/errorHandler');
 const NotFoundError = require('./errors/NotFoundError');
 
-const PORT = 3001;
+const PORT = 3000;
 
 // создали сервер
 const app = express();
